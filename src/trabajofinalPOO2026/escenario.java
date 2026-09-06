4c0a4f5535bde5936d7c3298f19a8af7032a0c84
Joaquin Luna <lunaesequielrosatio@gmail.com>
2026-09-06 16:57:53 -0300

    escenario y combate a reparar

---
+++ src/trabajofinalPOO2026/escenario.java
@@ -0,0 +1,16 @@
+package trabajofinalPOO2026;
+
+public class escenario {
+    protected int solido;
+    protected int tiempo;
+
+    public int suelosolido () {
+        
+        //esto se va a usar para comprobar que el suelo donde este el personaje es solido
+    }
+
+    public boolean tiempoCero () {
+        return tiempo = 0;
+        //Esto va a comprobar si el tiempo se termino/llego a 0
+    }
+}
